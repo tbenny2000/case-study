17d983826afedf1d1721ac4a9b02a801393b8347
tbenny2000 <tbenny2000@gmail.com>
2024-12-18 11:48:02 -0500

    start of case study project

---
+++ src/main/java/com/soccerstore/webapp/OnlineSoccerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.soccerstore.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSoccerStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSoccerStoreApplication.class, args);
+	}
+
+}
